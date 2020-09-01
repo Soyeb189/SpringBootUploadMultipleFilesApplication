a08a2dc29a17f00f120f7688eb1e736dacc11535
Soyeb189 <muktadir189@gmai.com>
2020-09-01 16:16:04 +0600

    First Commit

---
+++ src/test/java/com/soyeb/SpringBootUploadMultipleFilesApplication/SpringBootUploadMultipleFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soyeb.SpringBootUploadMultipleFilesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadMultipleFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
